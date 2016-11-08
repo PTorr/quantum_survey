3575b97d252cce96e41023c1ce7f5136471172b1
Torr <torrpolakow@gmail.com>
2016-11-08 21:50:49 +0200

    quantum state

--- quantum_state.py
+++ quantum_state.py
@@ -1,13 +1,16 @@
 import numpy as np
-def main():
-    d = np.sqrt(0.6)
+def main(d,d1):
+    # d = np.sqrt(0.6)
     c = np.sqrt(1-d*d)
-    d1 = np.sqrt(0.4)
+    # d1 = np.sqrt(0.4)
     c1 = np.sqrt(1 - d1 * d1)
     [a,b] = ab(c,c1,d,d1)
-    print ang(1)
     print('|s> = %s[%s|00>+%s|11>]+%s[%s|01>-%s|10>]' % ('a', 'c', 'd', 'b', 'c', 'd'))
     print('|s> = %.2f[%.2f|00>+%.2f|11>]+%.2f[%.2f|01>-%.2f|10>]'% (a,c,d,b,c,d))
+
+    # constrains:
+    # c * c + d * d = 1
+    # c1 * c1 + d1 * d1 = 1
 
 def coeffs_cd(a,b,c1,d1):
     # c =
@@ -29,4 +32,4 @@ def ang(a):
     return theta
 
 if __name__ == '__main__':
-    main()
+    main(0.6,0.4)
