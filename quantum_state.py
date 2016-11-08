fc1fd748189f12c7e17f6e854c394cb91b2d5add
Torr <torrpolakow@gmail.com>
2016-11-08 21:30:24 +0200

    quantum state

---
+++ quantum_state.py
@@ -0,0 +1,18 @@
+import numpy as np
+def main():
+    print ang(1)
+
+def coeffs_cd(c1,d1):
+    pass
+def coeffs_c1d1(c,d):
+    pass
+def coeffs_ab(c1, d1):
+    pass
+def ang(a):
+    # calculate the angle between the bits from a.
+    import math
+    theta = math.degrees(math.acos(a))
+    return theta
+
+if __name__ == '__main__':
+    main()
