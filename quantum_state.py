690365f79489be9f5cfbf06f8e83b0b643cfab7b
Torr <torrpolakow@gmail.com>
2016-11-08 21:45:26 +0200

    quantum state

--- quantum_state.py
+++ quantum_state.py
@@ -1,13 +1,27 @@
 import numpy as np
 def main():
+    d = np.sqrt(0.6)
+    c = np.sqrt(1-d*d)
+    d1 = np.sqrt(0.4)
+    c1 = np.sqrt(1 - d1 * d1)
+    [a,b] = ab(c,c1,d,d1)
     print ang(1)
+    print('|s> = %s[%s|00>+%s|11>]+%s[%s|01>-%s|10>]' % ('a', 'c', 'd', 'b', 'c', 'd'))
+    print('|s> = %.2f[%.2f|00>+%.2f|11>]+%.2f[%.2f|01>-%.2f|10>]'% (a,c,d,b,c,d))
 
-def coeffs_cd(c1,d1):
+def coeffs_cd(a,b,c1,d1):
+    # c =
+    # d =
+    # return c,d
     pass
-def coeffs_c1d1(c,d):
-    pass
-def coeffs_ab(c1, d1):
-    pass
+def coeffs_c1d1(a,b,c,d):
+    c1 = a*c+b*d
+    d1 = a*d-b*c
+    return c1, d1
+def ab(c,c1,d,d1):
+    a = c*c1+d*d1
+    b = d*c1-d1*c
+    return a,b
 def ang(a):
     # calculate the angle between the bits from a.
     import math
